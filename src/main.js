29047236f8f5244c149e592b7f3a7ca260d08d7a
Olesna22 <96056129+Olesna22@users.noreply.github.com>
2021-12-14 23:02:16 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -86,7 +86,7 @@ function signRequest(request, certPem) {
 		message.appendChild(el);
 	}
 	addToMessage("ID_Zpravy", uuidv4());
-	addToMessage("Verze", "201704D");
+	addToMessage("Verze", "201704E");
 	addToMessage("Odeslano", new Date().toJSON());
 	addToMessage("SW_Klienta", "Sukulent0000");
 
